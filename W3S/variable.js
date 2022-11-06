7321e399fe9e781012e785ddb461033a464e4590
0xsadik <torikussadik404@gmail.com>
2022-11-06 07:56:43 +0600

    variable

---
+++ W3S/variable.js
@@ -0,0 +1,69 @@
+
+/*
+
+        JavaScript varibales 
+
+        There is 4 ways to declare a javascript variable:
+            - using 'var'
+            - using 'let'
+            - using 'const'
+            - using nothing 
+
+
+        
+        What are varibales ?
+
+        variables are containers for storing data (storing data values)
+*/
+
+
+// var x = 3;
+// var y = 6;
+// var z = x + y;
+
+// another way 
+
+// var a, b, c;
+// a = 10;
+// b = 20;
+// c = a + b;
+
+// -- let 
+
+// let a, b, c;
+// a = 1;
+// b = 5; 
+// c = a * b;
+
+
+/*
+
+            When to Use JavaScript const?
+
+            if you want a general rule always declare variables with 'const'
+            if you think the value of the variable can change, use 'let'
+*/
+
+// const price1 = 50;
+// const price2 = 43;
+// let total = price1 + price2;
+
+
+
+// one satement, many variable 
+// let person = "torikus", carName = "Ferari", price = "$$$$$$$$$"
+
+
+
+//      value = undefined
+//      a variable declared without a value will have the value 'undefined'
+
+
+
+//      re-declaring javascript variables
+
+// var carname = "toyota";
+// var carname;
+
+
+
