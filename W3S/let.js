2d75e79634731bdd14fc09059c4fb2ef26e5c394
0xsadik <torikussadik404@gmail.com>
2022-11-06 21:23:50 +0600

    let

--- W3S/let.js
+++ W3S/let.js
@@ -25,3 +25,78 @@
 
 */
 
+
+
+// {
+//     let x = 2;
+// }
+
+// x can't be use here
+
+
+// {
+//     var x = 2;
+// }
+// x can be used here.
+
+
+
+/*
+
+                Re-declaring  variables
+
+                - Redeclaring a variable using the 'var' keyword can impose problems.
+                - Redeclaring a varibale inside a block will also redeclare the variable outside the block:
+
+
+                var x = 10;
+                // here x is 10 
+
+                {
+                    var x = 2;
+                    // here x is 2
+                }
+
+                // here x is 2;
+
+
+                - Redeclaring a variable using the 'let' keyword can solve this problem
+                - Redeclaring a variable inside a block will not redeclare the variable outside the clock:
+
+
+                let x = 10;
+                // here x is 10;
+
+                {
+                    let x = 2;
+                    // here x is 2
+                }
+                
+                // here x is 10
+
+
+
+*/
+
+
+
+/* 
+
+                    [+] Let Hoisting [+]
+
+                    - variable defined with 'var' are hoisted to the top and can be initialize at any time.
+                    meaning: you can use the variable before it is declared:
+
+                    example :
+                    This is ok:
+
+                    carName = 'volvo';
+                    var carName;
+
+
+
+            
+
+*/
+
+
