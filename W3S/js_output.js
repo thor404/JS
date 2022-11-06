8c16c9ca047ba6a90fde4d1dc616e4d6827ee210
0xsadik <torikussadik404@gmail.com>
2022-11-06 07:13:41 +0600

    js output

---
+++ W3S/js_output.js
@@ -0,0 +1,37 @@
+
+//  [+] javaScript Display possiilities [+]
+
+/*
+
+        javaScript can ' display data in different ways:
+            - innerHTML
+            - document.write()
+            - window.alert()
+            - console.log()
+
+*/
+
+// --> using innerHTML
+// document.getElementById("example").innerHTML = 12 + 4; 
+
+
+// --> using document.write()
+// document.write(5 + 6);
+
+
+// --> using window.alert()
+// window.alert(5 + 6)
+
+
+// we can skip window keyword
+// alert(5+4)
+
+// --> using console.log()
+// console.log("something here!");
+
+
+// --> JavaScript print 
+// button onclick = "window.print()"> print this page </button>
+
+
+
