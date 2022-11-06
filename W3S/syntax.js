334e1701290d03b0467e2005117d5516cf6963e2
0xsadik <torikussadik404@gmail.com>
2022-11-06 07:40:56 +0600

    syntax

---
+++ W3S/syntax.js
@@ -0,0 +1,81 @@
+// ====== javaScript syntax ======
+
+// to create variables:
+
+// var x;
+// var y;
+
+
+// using varibales 
+
+// x = 5;
+// y = 6;
+// let z = x + y;
+
+
+/* 
+
+        JavaScript Values:
+
+        the javascript syntax defines two types of values:
+                - Fixed values
+                - Variable values
+            
+            Fixed values are called ' literals'
+            variable values are called ' Variables ' 
+
+*/
+
+
+/*
+
+        --> js  literals <--
+            Numbers are written with or without decimals:
+
+                10.50   // with decimal 
+                1001    // without decimal (it's binary which decimal value is = 9)
+
+            Strings are text, written within double or single quotes:
+                "Shakil babu"
+                'Fahim morshed'
+
+
+*/
+
+
+/*
+
+            JavaScript Variables
+
+            in a programming language, varibales are used to store data values.
+
+            javascript uses the keywords (var, let, const) to declare varibales.
+
+            let x; --> where, let is a keyword and x is variable
+            x = 6; --> where, x is assigned (with '=' assignment operator) the value 6
+
+
+
+            JavaScript Operators:
+
+            javaScript uses 'arthmetic operators (+, -, *, /) to compute values
+
+            (10 + 20) * 5; 
+
+
+
+            JavaScript Expressions:
+
+            An expression is a combination of values, variables and operators
+
+            5 * 10
+            a * 5
+
+
+            All javascript identifires are 'case sensitive'
+            The vaiable 'LastName' and 'lastname' are two different variables
+
+            
+
+*/
+
