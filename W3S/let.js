a23542c5b3c3a1e992ef208820c4b650127b37d9
0xsadik <torikussadik404@gmail.com>
2022-11-06 14:19:12 +0600

    let

---
+++ W3S/let.js
@@ -0,0 +1,27 @@
+
+/* 
+
+            javaScript let 
+
+            - variable defined with 'let' cannot be redeclared.
+            - variable defined with 'let' must be declared before use.
+            - variable defined with 'let' have block scope 
+
+
+*/
+
+// let x = "fahishakil";
+// let x = 0;
+// console.log(x); // SyntaxError: Identifier 'x' has already been declared
+
+
+
+/*
+            Block Scope 
+
+            ES6 introduced two important new javascript keywords: let and const 
+            These two keywords provide 'block scope' in javascript 
+            varibale declared insed a {} block cannot be accessed from outsiede the block;
+
+*/
+
