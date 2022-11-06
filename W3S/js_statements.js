209dfdc1d18439ecea6c119c6934f15fce13551e
0xsadik <torikussadik404@gmail.com>
2022-11-06 07:23:27 +0600

    statements

---
+++ W3S/js_statements.js
@@ -0,0 +1,36 @@
+
+/* 
+
+            / / /  javaScript Statements / / / 
+            ----------------------------------
+
+
+*/
+
+// let a, b, c; // statement 1
+// a = 10;  // statement 2
+// b = 20; // statement 3
+// c = 30;  // statement 4
+
+
+/* 
+        Most javaScript programs contain many javaScript statements.
+        The statements are executed, one by one, in the same order as they are written.
+
+
+        Semicolons;
+
+        semicolons separate javascript statements.
+        when separated by semicolons, multiple statements on one line are allowed:
+        a = 12; b = 10; c = a + b; (multiple statements)
+
+
+        JavaScript white space:
+
+        javaScript ignores multiple spaces. You can add white space to your script to make it more readable.
+        -> a good practice is to put spaces around operators (+ - = / )
+
+        
+*/
+
+
