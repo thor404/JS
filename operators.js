821cbf956025b929659314a292438ead1d325570
0xsadik <torikussadik404@gmail.com>
2022-11-07 15:03:05 +0600

    js operators

---
+++ operators.js
@@ -0,0 +1,71 @@
+
+/*                  
+                     [+] javaScript Operators
+
+                     > the assignment operator(=) assigns a value to a variable
+                     > the addition operator (+) adds numbers:
+                     > the multiplication operator (*) multiplies numbers.
+                     
+
+
+                     [+] Types of javaScript operators
+
+                     There are different types of javaScript operators:
+
+                     - Arthmetic operators:
+                            +       addition
+                            -       subtraction
+                            *       multiplication
+                            **      exponentiation
+                            /       division
+                            %       modulus (division remainder)
+                            ++      increment
+                            --      decrement
+                        
+                        - Assignment operatos:
+                            =       x = y
+                            +=      x += y
+                            -=      x -= y
+                            *=      x *= y
+                            /=      x / y
+                            %=      x % y
+                            **=     x **= y
+
+                        - Comparison Operators:
+
+                            ==      equal to
+                            ===     equal value and equal type
+                            !=      not equal
+                            !==     not equal value or not equal type
+                            >       greater than
+                            <       less than
+                            >=      greater than or equal to
+                            <=      less than or equal to
+                            ?       ternary operator
+
+
+                        - Logical operators:
+                            &&      logical and
+                            ||      logical or
+                            !       logical not
+
+                        
+                        - Type operators:
+                            typeof          returns the type of a variable
+                            instanceof      returns true if an object is an instance of an object type
+                        
+
+                        - Bitwise Operators
+                            - bit operators work on 32bits number;
+
+                            &           AND
+                            |           OR
+                            ~           NOT
+                            ^           XOR
+                            <<          left shift
+                            >>          right shift
+                            >>>         unsigned right shift
+
+
+                        
+*/
