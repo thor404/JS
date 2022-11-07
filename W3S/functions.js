455170bdcb82aac1f8c0b893dabe0e68d13bb2c5
0xsadik <torikussadik404@gmail.com>
2022-11-07 16:04:30 +0600

    output

---
+++ W3S/functions.js
@@ -0,0 +1,13 @@
+
+/*
+                JavaScript Functions
+
+                a javascript function is a block of code designed to perform a particular task
+                a javascript function is executed when "something" invokes it (calls it).
+
+
+
+                example:
+
+                function
+*/
