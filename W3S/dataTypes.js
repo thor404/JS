16981e2cfe6b30f57623061b829031fe5c799297
0xsadik <torikussadik404@gmail.com>
2022-11-07 16:00:16 +0600

    data types

---
+++ W3S/dataTypes.js
@@ -0,0 +1,113 @@
+
+/**
+                [+] JavaScript Data Types 
+
+                javaScript vairables can hold different data types: numbes, string, objects and more:
+
+                let length = 12;                                // numeber
+                let lastName = "william";                       // string
+                let x = {firstName: "john", lastName: "Doe"};   // object
+
+
+
+                The concept of Data types:
+
+                - to be able to operate on variables it is important to know something about the type
+
+                
+                JavaScript types are dynamic
+
+                javascript has dynamic types. This means that the same variable can be used to hold different data types 
+
+                let x;              // x is undefined 
+                x = 5;              // x is a number 
+                x = "john"          // x is a string 
+
+
+
+
+                javaScript String:
+
+                a string is a series of characters like "fahim shakil"
+                string are written with quotes. we can use single or double quotes
+
+                let friend1 = "Shakil babu";        // using double quotes
+                let friend2 = 'Fahim morshed'       // using single quotes
+
+                single quotes inside double quotes:
+
+                let str = "his nick name is 'wolf' ";
+
+
+
+
+
+                javaScript Numbers
+                
+                javascript has only one type of numbers
+                numbers can be written with, or without decimals
+
+                let x1 = 34.00;
+                let x2 = 34;
+
+
+
+                // extra large or extra small numbers can be written with scientific (exponential) notation:
+
+                let y = 123e5;          // 12300000
+                let x = 123e-5          // 0.00123
+
+
+
+
+
+
+                javaScript Booleans
+
+                booleans can only have two values:
+                                - true
+                                - false
+
+                let x = 5;
+                let y = 5;
+                let z = 6;
+
+                (x == y)        // ture
+                (x == z)        // false
+
+
+
+
+                javaScript arrays:
+
+                javascript arrays are written with square brackets
+                array items are separated by commas
+
+                const cars = ['saab', 'vlovlo', 'BMW'];
+
+
+
+
+
+
+                javaScript Objects 
+
+                javascript objects are written with curly braces {}
+                object properties are written as name:value pairs, separated by commas;
+
+                const person = {firstname: 'shakil', lastname:'babu', age: 22, eyecolor: 'black'};
+
+
+
+
+
+                
+
+
+
+
+
+
+
+
+*/
