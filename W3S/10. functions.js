cf291f09955b8a69df08f7b36a30af5c8c985dd5
0xsadik <torikussadik404@gmail.com>
2022-11-08 14:53:01 +0600

    function

--- W3S/10. functions.js
+++ W3S/10. functions.js
@@ -5,9 +5,49 @@
                 a javascript function is a block of code designed to perform a particular task
                 a javascript function is executed when "something" invokes it (calls it).
 
+                function names can contain letters, digits, underscores, and dollar signs.
+                the parentheses may include parameter names separated by commas: (parameter1, parameter2);
+
+
 
 
                 example:
 
-                function
+                function myfunction(a , b) {
+                    return a + b;
+                }
 */
+
+
+/*
+                Function invocation 
+
+                The code inside the function will execute when "something" invokes(calls) the funciton:
+
+                        - when an event occurs (when a user clicks a button)
+                        - when it is invoked (called) from javaScript code
+                        - automatically (self invoked)
+
+*/
+
+
+
+/*
+                Fucntion Return 
+
+                    - when javaScript reaches a return satatement, the function will stop executing.
+                    - if the function was invoked from a statement, javaScript will  'return' to execute the code after the invoking statement 
+                    - function often compute a return value. the return value is 'returned' back to the 'caller'
+*/
+
+// example:
+
+// let x = myFunction(10, 20);         // here, function is called, return value will end up in x
+
+
+// function myFunction(a, b) {
+//     return a + b;                   // function returns the product of a and b
+// }
+
+
+
