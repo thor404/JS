3b0814ce712b97bb350892f840c1d31fae173479
0xsadik <torikussadik404@gmail.com>
2022-11-11 20:20:36 +0600

    BigInt

---
+++ W3S/Numbers/bigInt.js
@@ -0,0 +1,41 @@
+/*
+                    javascritp BigInt 
+                    javascript 'BigInt' variables are used to store big integer values that are too big to be represented by a normal javascript number
+*/
+
+//      javascript integer accuracey ( javascript int are only caccureate up to 15 digits)
+
+// let a = 999999999999999;
+
+
+//      how to create a BigInt
+//      to create a BigInt, append n to the end of an integer or call BigInt()
+
+
+// let x = 999999999999999;
+// let y = 999999999999999n;
+// console.log(x, y);
+
+
+
+// let big = BigInt(123455555444333222112343534242342);
+// console.log(big);
+// console.log(typeof(big));
+
+
+//      BigInt is the second numeric data type in javascript 
+
+
+
+//         ===== BigInt Decimals =====
+
+
+// let x = 5n;
+// let y = x / 2;
+// console.log(y);
+
+
+
+// let x = 5n;
+// let y = Number(x) / 2;
+// console.log(y);
