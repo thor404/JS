895bf2bf9994ae2abd62baf312febd4e30a40d78
0xsadik <torikussadik404@gmail.com>
2022-11-12 18:22:13 +0600

    number methods

---
+++ W3S/Numbers/numMethods.js
@@ -0,0 +1,109 @@
+/*
+                    javascript number methods
+                    these number methods can be used on all javascript numbers:
+
+                    toString()
+                    toExponetial()
+                    toFixed()
+                    toPrecision()
+                    ValueOf()
+*/
+
+
+//      toString() method
+
+// let x = 123;
+// let b = x.toString();
+// let c = (1234).toString();
+// let d = (100 + 24).toString();
+// console.log(x, b, c, d);
+
+
+//      toExponential method
+
+// let a = 9.455;
+// let b = a.toExponential(2);
+// let c = a.toExponential(4);
+// let d = a.toExponential(6);
+// console.log(a, b, c, d);
+
+
+
+//      toFixed() method
+
+// let a = 9.234;
+// let b = a.toFixed(0);
+// let c = a.toFixed(2);
+// let d = a.toFixed(6);
+// console.log(a, b, c, d);
+
+
+//      toPercision() method
+
+// let a = 9.232;
+// let b = a.toPrecision();
+// let c = a.toPrecision(2);
+// let d = a.toPrecision(4);
+// let e = a.toPrecision(6);
+// console.log(a, b, c, d, e);
+
+
+
+//      valueOf() method 
+
+// let x = 123;
+// let b = x.valueOf();
+// let c = (123).valueOf();
+// let d = (100 + 12).valueOf();
+// console.log(`${x}\n ${b}\n ${c}\n ${d}\n`);
+
+
+//      Number() method 
+
+
+// console.log(Number(true));
+// console.log(Number(false));
+// console.log(Number("10"));
+// console.log(Number("    10"));
+// console.log(Number("    10  "));
+// console.log(Number("    10.33  "));
+// console.log(Number("    10,33  "));
+// console.log(Number("    10  33  "));
+// console.log(Number("    john  "));
+
+
+
+
+
+//      parseInt() method 
+
+// console.log(parseInt("-10"));
+// console.log(parseInt("-10.33"));
+// console.log(parseInt("10"));
+// console.log(parseInt("10 20 30"));
+// console.log(parseInt("10 years"));
+
+
+//      parseFloat() method 
+
+// console.log(parseFloat("10"));
+// console.log(parseFloat("10.23"));
+// console.log(parseFloat("10 years"));
+
+
+
+
+//      Number.isInteger() method 
+
+// console.log(Number.isInteger(10));
+// console.log(Number.isInteger(10.45));
+
+
+//      Number.parseFloat() method 
+
+// console.log(Number.parseFloat("10"));
+// console.log(Number.parseFloat("10.33"));
+// console.log(Number.parseFloat("10 20 40"));
+// console.log(Number.parseFloat("10 years"));
+
+
