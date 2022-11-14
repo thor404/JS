ccca7ea1b1ae5cbb2043a753c75adc1474897393
0xsadik <torikussadik404@gmail.com>
2022-11-14 23:44:50 +0600

    date get methods

---
+++ W3S/Dates/date_get_methods.js
@@ -0,0 +1,67 @@
+
+/*
+                    javaScript get date methods 
+
+
+
+                    getFullYear()
+                    getMonth()
+                    getDate()
+                    getDay()
+                    getHours()
+                    getMinutes()
+                    getSeconds()
+                    getMilliseconds()
+                    getTime()
+*/
+
+
+
+//      getFullYear()
+
+// const d = new Date("2022-11-14");
+// console.log(d.getFullYear());
+
+
+
+
+//      getMonth() 
+
+// const d = new Date("2022-11-14"); 
+// console.log(d.getMonth());
+
+
+
+//      getDate()
+
+// const d = new Date("2022-11-14");
+// console.log(d.getDate());
+
+
+
+//      getMinutes()
+
+// const d = new Date();
+// console.log(d.getMinutes());
+
+
+
+//      getSeconds()
+
+// const d = new Date();
+// console.log(d.getSeconds());
+
+
+
+//      getMilliseconds()
+
+// const d = new Date();
+// console.log(d.getMilliseconds());
+
+
+
+
+//      getDay()
+
+// const d = new Date();
+// console.log(d.getDay());
