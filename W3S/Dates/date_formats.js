bc0938df59453dcfbe4f3f154b5a0cdb2fafe623
0xsadik <torikussadik404@gmail.com>
2022-11-14 22:28:50 +0600

    date formats

---
+++ W3S/Dates/date_formats.js
@@ -0,0 +1,32 @@
+/*
+                javascript Date formats
+
+                thre are 3 types of js date input formats:
+
+                ISO Date
+                Short Date
+                Long Date
+*/
+
+
+//      javaScript ISO dates:
+
+// const d = new Date("2022-11-14");
+// console.log(d);
+
+
+//      ISO Dates (Year and Month)
+
+// const date = new Date("2022-11");
+// console.log(date);
+
+
+//      ISO Dates (Only Year)
+
+// const date = new Date("2022");
+// console.log(date);
+
+
+
+
+//      .... 
