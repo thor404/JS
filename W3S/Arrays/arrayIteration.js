5595547366e35161a770a8424ad274aebb59f54e
0xsadik <torikussadik404@gmail.com>
2022-11-14 21:14:40 +0600

    array iteration

--- W3S/Arrays/arrayIteration.js
+++ W3S/Arrays/arrayIteration.js
@@ -52,6 +52,112 @@
 
 
 
+/*
+                    javascript Array reduce();
+
+                    the reduce() method runs a function on each array element to produce ( reduce it to ) a single value ;
+                    the reduce() method works from left-to-right in the array. 
+*/
+
+// const numbers = [45, 4, 9, 16, 25];
+// let sum = numbers.reduce(myFunction);
+
+// function myFunction(total, value, index, array) {
+//     return total + value;
+// }
+// console.log(sum);
+
+
+
+
+// const numbers = [45, 4, 9, 16, 25];
+// let sum = numbers.reduce(myFunction, 10);       // we can set an initial value 
+
+// function myFunction(total, value) {
+//     return total + value;
+// }
+// console.log(sum);
+
+
+
+
+/*
+                    Array.every()
+
+                    the every() method check if all array values pass a test
+*/
+
+
+// const numbers = [45, 4, 9, 16, 25];
+// let allOver18 = numbers.every(myFunction);
+
+// function myFunction(value, index, array) {
+//     return value > 1;
+// }
+
+// console.log(allOver18);
+
+
+
+/*
+                    javascript array some()
+                    The some() method checks if some array values pass a test 
+*/
+
+
+// const numbers = [45, 4, 9, 16, 25];
+// let allOver18 = numbers.some(myFunction);
+
+// function myFunction(value) {
+//     return value > 18;
+// }
+
+// console.log(allOver18);
+
+
+
+/*
+                    javaScript array indexOf()
+
+                    The indexOf() method searches an array for an element value and returns its position
+*/
+
+// const frts = ["Apple", "Orange", "Apple", "Mango"];
+// let position = frts.indexOf("Apple") + 1;
+// console.log(position);
+
+
+
+
+/*
+                    javascript array lastIndexOf()
+
+                    Array.lastIndexOf() is the same as indexOf() but returns the position of the last occurance of the specified element
+*/
+
+// const frts = ["Apple", "Orange", "Apple", "Mango", "Apple", "apple"];
+// let position = frts.lastIndexOf("Apple");
+// console.log(position);
+
+
+
+/*
+                    JavaScript Array find()
+
+                    The find() method returns the value of the first array element that passes a test function
+*/
+
+// const numbers = [4, 9, 16, 25, 29];
+// let first = numbers.find(myFunction);
+
+// function myFunction(value) {
+//     return value > 18;
+// }
+
+// console.log(first);
+
+
+
 
 
 
