a419b6530b236e6cb508f1b4d4d255cc58c916f9
0xsadik <torikussadik404@gmail.com>
2022-11-14 20:39:02 +0600

    array iteration

---
+++ W3S/Arrays/arrayIteration.js
@@ -0,0 +1,57 @@
+
+// const numbers = [45, 4, 9, 16, 25];
+
+// function myFunction(value, index, array) {
+//     console.log(value);
+//     console.log(index);
+//     console.log(array);
+// }
+
+// // array methds 
+
+// numbers.forEach(myFunction)
+
+
+/*
+                    javaScript Array map()
+
+                    The map() method creates a new array by performing a function on each array element
+
+                    The map() method does not execute the function for array elements without values 
+
+                    The map() method does not change the original array
+*/
+
+
+// const numbers1 = [45, 4, 9, 16, 25];
+// const numbers2 = numbers1.map(myFunction);
+
+// function myFunction (value, index, array) {
+//     return value * 2;
+// }
+
+// console.log(numbers2);
+
+
+
+
+/*
+                    javascript Array filter()
+
+                    The filter() method creates a new array with array elements that pass a test
+*/
+
+
+// const numbers = [45, 4, 9, 16, 25];
+// const over18 = numbers.filter(myFunction); 
+
+// function  myFunction(value, index, array) {
+//     return value > 18;
+// }
+// console.log(over18);
+
+
+
+
+
+
