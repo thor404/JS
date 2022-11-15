9e5dcfa64b3243b1f52ec51be3156cdfbbebaf19
0xsadik <torikussadik404@gmail.com>
2022-11-15 22:18:30 +0600

    while

---
+++ W3S/Loop/while.js
@@ -0,0 +1,31 @@
+
+// while (condition) {
+//     // code block to be executed
+//   }
+
+
+
+// let i = 0;
+// while (i < 10) {
+//     console.log(i);
+//     i++;
+// }
+
+// do while
+
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// }
+// while (i < 9);
+
+
+
+// const cars = ["BMW", "Volvo", "Saab", "Ford"];
+// let v = 0;
+// while (cars[v]) {
+//     console.log(cars[v]);
+//     v++;
+// }
+
