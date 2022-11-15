7047818925bb383edfdbd74de9154bd808b68d97
0xsadik <torikussadik404@gmail.com>
2022-11-15 20:21:19 +0600

    comparison

---
+++ W3S/Booleans/booleans.js
@@ -0,0 +1,26 @@
+/*
+                    javaScript booleans 
+
+                    A javascript boolean represents one of two values : 'true' or 'false'
+*/
+
+
+// console.log(Boolean(10 > 9));
+// console.log(Boolean(10 > 19));
+
+
+
+
+// let a = 0;
+// let b = Boolean(a);
+// console.log(b);
+
+
+// let str = "";
+// console.log(Boolean(str));
+
+
+// let n = null;
+// console.log(Boolean(n));
+
+
