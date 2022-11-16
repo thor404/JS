7e886617ca25f8b26063e4831528f709d0a0397d
0xsadik <torikussadik404@gmail.com>
2022-11-16 07:15:27 +0600

    typeof

---
+++ W3S/TypeOf/typeOf.js
@@ -0,0 +1,42 @@
+
+/*
+                    javascript typeof 
+
+                    in javascript there are 5 different data types that can contain values:
+                            . string 
+                            . number 
+                            . boolean
+                            . object 
+                            . function
+
+                    there are 6 types of objects:
+                            . Object
+                            . Date
+                            . Array
+                            . String
+                            . Number
+                            . Boolean
+
+                    add 2 data types that cannot contain values:
+                            . null
+                            . undefined 
+*/
+
+
+// console.log(typeof 'john');
+// console.log(typeof 2.32);
+// console.log(typeof NaN);
+// console.log(typeof false);
+// console.log(typeof {name: 'tori', age: 23});
+// console.log(typeof new Date());
+// console.log(typeof function() {});
+// console.log(typeof myCar);
+// console.log(typeof null);
+
+
+
+// console.log('tori'.constructor);
+// console.log(3.12.constructor);
+// console.log(function() {}.constructor);
+
+
