ed6f52f808a4e39c591d58b1517a786dbebfce84
0xsadik <torikussadik404@gmail.com>
2022-11-16 07:43:22 +0600

    type conversio

---
+++ W3S/TypeConv/typeCoversion.js
@@ -0,0 +1,39 @@
+
+
+
+//          converting string to number 
+
+// let str = '123';
+// let num = Number(str);
+// console.log(num);
+
+
+
+
+// let str = '12.20';
+// let num = parseFloat(str);
+// console.log(num);
+
+
+
+// let str = '12.22';
+// let num = parseInt(str);
+// console.log(num);
+
+
+
+//          the unary + operator can be used to convert a varibale to a number 
+
+// let str = '10';
+// let num = + str;
+// console.log(num);
+
+
+//          converting numbers to string 
+
+// let num = 12;
+// let str = String(num);
+// console.log(str);
+
+
+//  ->.....
