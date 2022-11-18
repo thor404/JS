11fe9e4e602a8ab450936203a6e677d841224768
0xsadik <torikussadik404@gmail.com>
2022-11-18 08:47:17 +0600

    obj 2

---
+++ jsEssential/obj2.js
@@ -0,0 +1,19 @@
+const backpack = {
+    name: 'everyday backpack',
+    volume: 30,
+    color: 'grey',
+    pocketNum: 15,
+    strapLength: {
+        left: 26,
+        right: 26,
+    },
+    lidOpen: false,
+    toggleLid: function(lidStatus) {
+        this.lidOpen = lidStatus;
+    },
+    newStrapLength: function(lengthLeft, lengthRight) {
+        this.strapLength.left = lengthLeft;
+        this.strapLength.right = lengthRight;
+    },
+};
+
