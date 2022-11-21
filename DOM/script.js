7d9731e7a0b107bd25ec5aebb8fb9914477e18d3
0xsadik <torikussadik404@gmail.com>
2022-11-21 10:39:34 +0600

    DOM

---
+++ DOM/script.js
@@ -0,0 +1,21 @@
+// console.log(document.title);
+// console.log(document.URL);
+// console.log(document.domain);
+// console.log(document.head);
+// console.log(typeof(document.head));
+// console.dir(document.head);
+// console.log(document.body);
+
+// document.title = 'play with dom';
+
+// for (let element of document.all) {
+//     console.log(element);
+// }
+
+
+// console.log(document.getElementById('new-task'));
+
+
+let headElement = document.getElementById('header');
+headElement.textContent = 'To Do apps';
+headElement.innerText = 'To do apps';
